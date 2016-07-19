dc41f8f2cff73f02761a3dff40e582605746c614
Grey Panther <dify.ltd@gmail.com>
2016-07-19 17:35:42 +0000

    Eventbrite API WIP
    See:
    - https://groups.google.com/forum/#!topic/eventbrite-api/4dhSv6gr1fg
    - http://www.eventbrite.com/o/asociatia-ip-workshop-6671021543
    - https://www.eventbriteapi.com/v3/events/search/?organizer.id=6671021543&include_unavailable_events=true

---
+++ update-from-eventbrite.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import utils
+
+
+print(utils.eventbriteApi('events/search/?organizer.id=6671021543&include_unavailable_events=true'))
--- utils.py
+++ utils.py
@@ -11,3 +11,10 @@ def meetupApi(url):
   else:
     url += '?key=%s' % key
   return requests.get(url).json()
+
+
+def eventbriteApi(url):
+  eventbrite_key = os.environ['EVENTBRITE_API_KEY']
+  return requests.get(
+    'https://www.eventbriteapi.com/v3/%s' % url, headers={'Authorization': 'Bearer %s' % eventbrite_key}).json()
+
